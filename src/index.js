afd150f772b17702ff4c3ddb912364d238c5fba6
Mit <mitsonani7777@gmail.com>
2024-11-29 21:50:33 +0530

    Feat:User model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { ApolloServer } from '@apollo/server';
 import express from 'express';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db.js'
+
 
 async function init() {
     const app = express();
@@ -13,12 +15,21 @@ async function init() {
             hello: String
             say(name:String):String
         }
+        type Mutation{
+        createUser(firstName:String!,lastName:String!,email:String!,password:String!):Boolean
+        }
         `,
         resolvers: {
             Query: {
                 hello: () => `hey there`,
                 say: ("", name => `hey ${name}`)
             },
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password }) => {
+                    await prismaClient.user.create({ data: { email, firstName, lastName, password, salt: "hello" } });
+                    return true;
+                }
+            }
         },
     });
 
